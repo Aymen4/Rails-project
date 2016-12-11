d46dba5d23f92d724575c4f9b6221e429feca2b7
Aymen4 <Aymen@Aymens-Mac.lan>
2016-12-11 23:46:00 +0100

    added restController for Shortoners so we know can create a url_shortoner

--- URL-shortener/config/routes.rb
+++ URL-shortener/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'home#index'
-
-  resources :posts
-
   devise_for :users, controllers: {registrations: "registrations"} 
 
+  get '/shortoners' => 'shortoners#index'
+  get '/shortoners/new' => 'shortoners#new'
+	
+  post 'shortoners' => 'shortoners#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
