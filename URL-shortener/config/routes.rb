3b232cbefd0f0d24b6514b5068eae0d63b6e0d29
Aymen4 <Aymen@Aymens-Mac.lan>
2016-12-13 01:04:54 +0100

    now able to redirect to an external website with short_url

--- URL-shortener/config/routes.rb
+++ URL-shortener/config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: "registrations"} 
 
   get '/shortoners' => 'shortoners#index'
-  get '/shortoners/new' => 'shortoners#new'
+  
+  get '/shortoners/:short_url' => 'shortoners#show'
+
+  get '/shortoners/new/new' => 'shortoners#new'
 	
   post 'shortoners' => 'shortoners#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
